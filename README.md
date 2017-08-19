ce113c4a9690686aa042f6f19bec97745f82f6c6
ajilisiwei <ajilisiweiwei@126.com>
2017-08-19 22:51:31 +0800

    add api

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # dev_template_front_end
+0.接口文档
+http://www.xiaoyaoji.cn/dashboard/#!/project/PiYiBgtYx
+
 1.前端代码
+npm install
+bower install
+node ./bin/www
+
+2.代码编辑是用supervisor，能实现自动检测刷新
+supervisor ./bin/www
---
+++ routes/apis.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/water', function(req, res, next) {
+    var result={
+        "error_msg":null,
+        "status":0,
+        "data":[
+            {
+                "city_name":"深圳",
+                "city_id":1,
+                "type":1,
+                "data":[0.9,1.5,2.3,3.5,3.0,3,4,3.0,2.1,4.0,4.5,3.4]
+            },
+            {
+                "city_name":"深圳",
+                "city_id":1,
+                "type":2,
+                "data":[1,2.3,3.0,4.0,3.5,3.0,4.0,5.5,3.0,4.3,5.0,3.8]
+            }
+        ]
+    };
+    res.json(result)
+});
+
+
+module.exports = router;
